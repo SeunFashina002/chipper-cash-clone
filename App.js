043c1275dc0952131f96489c6e5ee0bf7a04fce3
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-02-25 11:12:27 -0800

    imported new components

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
+import styles from './styles';
+import { HomeHeader } from './components/HomeHeader';
+import { Hero } from './components/Hero';
+import { Transactions } from './components/Transactions';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.subContainer}>
+        <HomeHeader />
+        <Hero />
+        <Transactions/>
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
