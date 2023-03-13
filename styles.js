a77722081702f9e276d1f377af544ea1f41a2988
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-13 13:49:31 -0700

    added more styling to other screens

--- styles.js
+++ styles.js
@@ -177,6 +177,200 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
+  // Referral Screen
+  referralSubContainer: {
+    // borderWidth: 2,
+    paddingHorizontal: 4,
+    width: "100%",
+    height: 350,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+
+  referralImageContainer: {
+    width: "50%",
+    height: "55%",
+    marginVertical: 12,
+  },
+
+  referralImage: {
+    width: "100%",
+    height: "100%",
+  },
+
+  referralHeaderText: {
+    fontWeight: "600",
+    fontSize: 35,
+    marginVertical: 20,
+  },
+  referralText: {
+    textAlign: "center",
+    fontSize: 17,
+  },
+
+  earnBtnsSubContainer: {
+    // borderWidth: 2,
+    width: "100%",
+    height: 275,
+    paddingHorizontal: 5,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+  },
+
+  earnBtnsContainer: {
+    width: "100%",
+    height: "20%",
+    // borderWidth: 1,
+    marginVertical: 4,
+  },
+
+  earnBtns: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  // stock screen
+
+  stockScreenImageContainer: {
+    paddingHorizontal: 6,
+    // borderWidth: 2,
+    width: "100%",
+    maxHeight: 350,
+    height: "65%",
+  },
+
+  // stockScreenImageInnerContainer: {
+  //   borderWidth: 2,
+  //   width: "100%",
+  //   height: "100%",
+  // },
+
+  stockScreenImage: {
+    width: "100%",
+    height: "100%",
+    // borderWidth: 5,
+  },
+
+  // stock data
+  stockDataContainer: {
+    marginTop: 10,
+    flexDirection: "column",
+    // borderWidth: 1,
+    // paddingLeft: 12,
+  },
+  stockData: {
+    flexDirection: "row",
+    // borderWidth: 1,
+    height: 70,
+    padding: 6,
+    alignItems: "center",
+    marginVertical: 4,
+  },
+  stockLogoContainer: {
+    width: "10%",
+    height: "70%",
+    // borderWidth: 1,
+    marginRight: 15,
+  },
+  stockLogo: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 100,
+  },
+  stockTextContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "90%",
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    // borderWidth: 1,
+    borderBottomColor: "#e5e7eb",
+  },
+
+  // cards screen styling
+  cardToggleSwitchContainer: {
+    flexDirection: "row",
+    borderRadius: 7,
+    borderWidth: 1,
+    borderColor: "#f1f5f9",
+    marginTop: 6,
+  },
+
+  cardToggleSwitch: {
+    paddingVertical: 5,
+    paddingHorizontal: 3,
+    width: "50%",
+    backgroundColor: "white",
+    borderRadius: 7,
+    shadowOffset: 6,
+  },
+
+  bgImgContainer: {
+    width: "100%",
+    height: "50%",
+    marginTop: 5,
+    marginBottom:'auto',
+  },
+
+  bgImg: {
+    flex: 1,
+    // justifyContent:"center",
+  },
+
+  card: {
+    marginTop: 32,
+    flexDirection: "column",
+    backgroundColor: "black",
+    width: "100%",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    height: "60%",
+    borderRadius: 12,
+  },
+
+  cardHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 50,
+    marginBottom: "auto",
+    // borderWidth: 1,
+    // borderColor: "white",
+  },
+  cardHeaderLogoContainer: {
+    width: "50%",
+    height: "100%",
+    marginRight: "auto",
+  },
+  cardHeaderLogo: {
+    width: "100%",
+    height: "100%",
+  },
+
+  cardFooter: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    // borderWidth: 1,
+    // borderColor: "white",
+    height: "60%",
+    marginBottom:12,
+  },
+
+  cardFooterLogoContainer: {
+    width: "25%",
+    height: "20%",
+    marginLeft: "auto",
+  },
+  cardFooterLogo: {
+    width: "100%",
+    height: "100%",
+  },
 });
+
+
+
 
 export default styles;
