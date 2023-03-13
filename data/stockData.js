10b4557f000c4b0ac744d04a39942d8a86de8003
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-13 14:03:18 -0700

    Created mock data to mimic stocks

---
+++ data/stockData.js
@@ -0,0 +1,39 @@
+
+
+export const DATA = [
+  {
+    company: "Adobe Systems Inc.",
+    img: require("../assets/adobe.png"),
+    price: "$344.95",
+  },
+  {
+    company: "Apple, Inc.",
+    img: require("../assets/apple.png"),
+    price: "$151.43",
+  },
+  {
+    company: "Oracle Corp.",
+    img: require("../assets/oracle2.png"),
+    price: "$85.96",
+  },
+  {
+    company: "HP Inc.",
+    img: require("../assets/hp.png"),
+    price: "$28.49",
+  },
+  {
+    company: "Chevron Corporation.",
+    img: require("../assets/chevron.png"),
+    price: "$162.30",
+  },
+  {
+    company: "Microsoft Inc.",
+    img: require("../assets/microsoft.png"),
+    price: "$93.71",
+  },
+  {
+    company: "Google Inc.",
+    img: require("../assets/google.png"),
+    price: "$143.69",
+  },
+];
