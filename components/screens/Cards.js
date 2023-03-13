5e67f9d2f52c0ec637f03d1047cc43317a463a58
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-03-13 13:55:09 -0700

    Created the card screen

--- components/screens/Cards.js
+++ components/screens/Cards.js
@@ -1,12 +1,75 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, Pressable, TouchableOpacity } from "react-native";
 import styles from "../../styles";
+import { Ionicons } from "@expo/vector-icons";
 
-export const CardScreen = () => {
+export const Card = ({bgColor, currency, symbol, acct_no, amount}) => {
+  const chipper = require('../../assets/card_logo.png')
+  const visa = require("../../assets/visa.png");
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 20, fontWeight: "900" }}>
-        The Virtual Cards Screen is coming soon! Work in progress💖💞🧡😍
-      </Text>
-    </View>
+    <>
+      <View style={{ ...styles.card, backgroundColor:bgColor}}>
+        <View style={styles.cardHeader}>
+          <View style={styles.cardHeaderLogoContainer}>
+            <Image source={chipper} style={styles.cardHeaderLogo}></Image>
+          </View>
+
+          <Text style={{ color: "white", fontWeight: "700" }}>{currency}</Text>
+        </View>
+
+        <View style={styles.cardFooter}>
+          <View>
+            <Text style={{ color: "white" }}>Oluwaseun Fashina</Text>
+            <Text style={{ color: "white" }}>{acct_no}</Text>
+          </View>
+
+          <View style={styles.cardFooterLogoContainer}>
+            <Image style={styles.cardFooterLogo} source={visa}></Image>
+          </View>
+        </View>
+      </View>
+
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <TouchableOpacity>
+          <Text style={{ fontSize: 18, color: "#3b82f6", fontWeight: "600" }}>
+            Show card details
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Ionicons
+            name="information-circle-outline"
+            size={32}
+            color="#3b82f6"
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 16,
+          paddingHorizontal: 24,
+        }}
+      >
+        <Text style={{ fontSize: 18, fontWeight: "800" }}>
+          {" "}
+          CURRENT BALANCE
+        </Text>
+        <View style={{ flexDirection: "row" }}>
+          <Text
+            style={{ alignSelf: "flex-end", fontSize: 20, fontWeight: "700" }}
+          >
+            {symbol}
+          </Text>
+          <Text style={{ fontSize: 48, fontWeight: "700" }}>{amount}</Text>
+        </View>
+        <Text style={{ textAlign: "center", color: "gray", fontWeight: "500" }}>
+          Fund your Chipper Card below using funds from your Chipper Wallet
+        </Text>
+      </View>
+    </>
   );
 };
+
+
